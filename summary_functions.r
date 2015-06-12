2866f412153366967af700f8a774be98528e6a75
Allen Hurlbert <hurlbert@bio.unc.edu>
2015-06-12 15:53:45 -0400

    new script for analyzing CC data

---
+++ summary_functions.r
@@ -0,0 +1,13 @@
+################################################
+# Script for conducting basic analyses based on 
+# Caterpillars Count! data exported from the
+# phpMyAdmin project site.
+
+# Data include:
+#
+# tbl_surveys: date, site, time, and temperature of all survey events
+#
+# tbl_orders: arthropod observed, their length, and count associated with
+#             each survey event
+
+
