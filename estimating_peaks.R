2013b7969363ada5439b867174e4c6c82c6fbdca
Tracie Hayes <hayeste@live.unc.edu>
2015-09-30 14:58:26 -0400

    script for estimating peaks w varying method/#

---
+++ estimating_peaks.R
@@ -0,0 +1,9 @@
+# Script for estimating peaks based on varying surveying methods and varying
+# number of surveys a month.
+# Both quadratic and cubic fits.
+
+# Run summary_functions.r, data_cleaning.R, general_plotting.R
+source('summary_functions.r')
+source('data_cleaning.R')
+source('general_plotting.R')
+
