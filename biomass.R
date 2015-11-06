66523eb07db3ea4c5f7e25faea097616d138f728
hayeste <hayeste@live.unc.edu>
2015-11-06 15:46:53 -0500

    fixed for loop

--- biomass.R
+++ biomass.R
@@ -1,6 +1,6 @@
 # Script for calculating biomass and adding this as column to the cleandata
 
-# Create empty biomass column
+# Create empty biomass vector
 cleandata$biomass = NA
 
 # y = a(x)^b
@@ -10,7 +10,7 @@ reg.data.temp <- read.csv('arth_regression.csv', header = T, sep = ',')
 reg.data.temp$coefficient <- 10^(reg.data.temp$intercept)
 
 # Create list of arthropod orders (by code)
-arthlist <- arthcodes$ArthCode # arthcodes from data_cleaning.R
+arthlist <- as.vector(arthcodes$ArthCode) # arthcodes from data_cleaning.R
 
 # Merge reg.data.temp and arthlist so NAs will be calculated
 reg.data <- merge(reg.data.temp, arthcodes, by.x = 'arthCode', by.y = 'ArthCode', all = T)
@@ -19,5 +19,16 @@ reg.data <- merge(reg.data.temp, arthcodes, by.x = 'arthCode', by.y = 'ArthCode'
 for (ord in arthlist) {
   b = reg.data[reg.data$arthCode == ord,]$slope
   a = reg.data[reg.data$arthCode == ord,]$coefficient
-  cleandata$biomass[cleandata$arthCode == ord,] = (a*(cleandata$length[cleandata$arthCode == ord,])^(b))
+  cleandata$biomass[cleandata$arthCode == ord] <- (a*(cleandata$length[cleandata$arthCode == ord])^(b))*(cleandata$count[cleandata$arthCode == ord])
 }
+
+# Orders with regression data:
+regorders <- as.vector(reg.data.temp$arthCode)
+
+# Trying meanDensityByDay function
+biomass.prbs <- meanDensityByDay(cleandata, ordersToInclude = regorders, minLength = 5, inputSite = 117, 
+                                 inputYear = 2015, plot = T, plotVar = 'meanBiomass')
+
+
+
+
