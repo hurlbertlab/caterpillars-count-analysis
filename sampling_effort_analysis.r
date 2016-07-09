fc7002a6c9fc45e3de160fffdfbd5ee0bdf3dc7e
Allen Hurlbert <hurlbert@bio.unc.edu>
2016-07-09 16:40:29 -0400

    working on divergent color ramp for deviations but not there yet

--- sampling_effort_analysis.r
+++ sampling_effort_analysis.r
@@ -43,8 +43,8 @@ for (cir in 1:8) { # survey circle effort loop
     for (e in 1:(d-1)) { # alternative starting date for a given freq loop
       # Starting on the 2nd sample date since the 1st one 
       # during training only has 10 surveys
-      dateseq = seq(e+1, nrow(fixedEffort), by = d)
-      sampledates = as.character(fixedEffort$date[dateseq])
+      dateseq = seq(e+1, nrow(BGeffortByDay), by = d)
+      sampledates = as.character(BGeffortByDay$date[dateseq])
             
       effort = BGeffortByDay[BGeffortByDay$date %in% sampledates, ]
       
@@ -82,24 +82,36 @@ JDmeanmat = matrix(foo$JDmean, nrow = length(unique(foo$circles)),
                   byrow = TRUE)
 
 # Deviations from most frequent, intense sampling
-maxJDmat1 = maxJDmat - maxJDmat[8, 6]
-JDmeanmat1 = JDmeanmat - JDmeanmat[8, 6]
+maxJDmat1 = maxJDmat - maxJDmat[8, 6]     #check, is this the right value?
+JDmeanmat1 = JDmeanmat - JDmeanmat[8, 6]     #check, is this the right value?
+
+divergecol = function(values, bins, neg_col, pos_col) {
+  col5 = colorRampPalette(c(neg_col, 'white', pos_col))
+  color_levels = bins
+  max_absolute_value = max(abs(values), na.rm = TRUE)
+  color_sequence=seq(-max_absolute_value,max_absolute_value,length.out=color_levels+1)
+}
 
 # Plotting
-col1 = 'blue'
-col2 = 'red'
+pos_col = 'blue'
+neg_col = 'red'
+colRamp = colorRampPalette(c(neg_col, 'white', pos_col))
 colfunc = colorRampPalette(c(col1, col2))
 
-sampPlot = function(sampMatrix, col1, col2, title) {
+
+sampPlot = function(sampMatrix, colRamp, bins, title) {
+  max_absolute_value = max(abs(sampMatrix), na.rm = TRUE)
+  color_sequence=seq(-max_absolute_value,max_absolute_value, length.out=bins+1)
+  
   par(mgp = c(1.5, 0.3, 0), mar = c(3, 3, 2, 2))
   layout(matrix(1:2,ncol=2), width = c(2,1),height = c(1,1))
   image(sampMatrix, ylab = "Sampling frequency (#/month)", 
         xlab = "Number of surveys", main = title, 
-        xaxt = "n", yaxt = "n", col = colfunc(20))
+        xaxt = "n", yaxt = "n", col = colRamp(bins))
   axis(2, seq(0, 1, length.out = 6), labels = round(8/(6:1), 1), tck = -.01)
   axis(1, seq(0, 1, length.out = 8), labels = 5*(1:8), tck = -.01, las = 1)
   
-  legend_image <- as.raster(matrix(colfunc(20), ncol=1))
+  legend_image <- as.raster(matrix(colRamp(bins), ncol=1))
   plot(c(0,2),c(0,1),type = 'n', axes = F,xlab = '', ylab = '')
   mtext(seq(round(max(sampMatrix)), round(min(sampMatrix)), length.out=3), 4, 
         at = seq(0,1,l=3), las = 1, line = -1)
