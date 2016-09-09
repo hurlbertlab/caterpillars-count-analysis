581f885b8ada8d9ce31c9d50f4f25861604065a5
Tara McKinnon <tmckinno@live.unc.edu>
2016-09-09 11:11:56 -0400

    added anova to compare effect of site on arth dens

--- Exclosure Analysis/exclosure_model.R
+++ Exclosure Analysis/exclosure_model.R
@@ -4,25 +4,26 @@ library(MASS)
 library(ggplot2)
 library(lme4)
 source(ExclosureAnalysis_Summer2016.R)
+
 #Figure out which probability distribution fits data -None of the following are very good (all for normal distributions)
-food_time$Visit3.1 <- food_time$Visit3 + .1
-qqp(food_time$Visit3.1, "norm")
-qqp(food_time$Visit3.1, "lnorm")
-nbinom <- fitdistr(food_time$Visit3.1, "Negative Binomial")
-qqp(food_time$Visit3.1, "nbinom", size = nbinom$estimate[[1]], mu = nbinom$estimate[[2]])
-poisson <- fitdistr(food_time$Visit3.1, "Poisson")
-qqp(food_time$Visit3.1, "pois", poisson$estimate)
-gamma <- fitdistr(food_time$Visit3.1, "gamma")
-qqp(food_time$Visit3.1, "gamma", shape = gamma$estimate[[1]], rate = gamma$estimate[[2]])
+#food_time$Visit3.1 <- food_time$Visit3 + .1
+#qqp(food_time$Visit3.1, "norm")
+#qqp(food_time$Visit3.1, "lnorm")
 
 #data shaping
-food <- food_time1
-food$identifier <- paste0(food$siteID, food$circle, food$survey)
+food = food_time1
+food$identifier = paste0(food$siteID, food$circle, food$survey)
 food = merge(food, all_surveyTrees, by.x="identifier", by.y= "identifier")
 food = select(food, -siteID.y, -circle.y, -survey.y)
 names(food)= c("identifier", "TrapType", "siteID", "survey", "circle", "food_sum", "surveyTrees")
 
+#mixed model #why am I getting 0 values for the surveyTrees?
 mix_mod = lmer(food_sum ~ TrapType + (1 | surveyTrees) + (1 | siteID), food)
-anova(mix_mod)
-mix_gmod = glmer(food_sum ~ TrapType + (1 | surveyTrees), food)
+mix_mod1 = lmer(food_sum ~ TrapType + (1 | surveyTrees), food)
+
+#is there an effect of site
+anova(mix_mod, mix_mod1)
+
+
+#mix_gmod = glmer(food_sum ~ TrapType + (1 | surveyTrees), food)
 
