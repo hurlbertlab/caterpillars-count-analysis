865dd686b46f6d99e0133f38364a7564a2d36996
Tara McKinnon <tmckinno@live.unc.edu>
2016-09-16 13:14:03 -0400

    reshaped 2012 data to allow for modeling and began running potential mixed effects models

--- Exclosure Analysis/exclosure_model.R
+++ Exclosure Analysis/exclosure_model.R
@@ -1,6 +1,7 @@
 #Mixed Effects Model for Arth Density
 library(ggplot2)
 library(lme4)
+
 source("Exclosure Analysis/ExclosureAnalysis_Summer2016.R")
 source("~/Desktop/insect-exclosure/data_analysis.R")
 
@@ -24,11 +25,21 @@ mix_mod_food = lmer(food_sum ~ TrapType + (1 | surveyTrees), food)
 mix_mod_caterpillar = lmer(caterpillar_sum ~ TrapType + (1 | surveyTrees), caterpillar)
 
 #data shaping for 2012
-food_2012 = food_time
-surveyTrees_A = select(topdown3, StateRouteStop, Station, TrapType, TreeSpecies)
+food_new = food_time
+food_new$identifier = paste0(food_new$StateRouteStop, food_new$Station, food_new$TrapType) 
 
+#merge tree species data with density data
+uniqTrees = unique(topdown9[, c('StateRouteStop', 'Station', 'TrapType', 'VisitNumber', "TreeSpecies")])
+uniqTreesV3 = uniqTrees[uniqTrees$VisitNumber =="3",]
+uniqTreesV3$identifier = paste0(uniqTreesV3$StateRouteStop, uniqTreesV3$Station, uniqTreesV3$TrapType) 
+food_2012 = merge(food_new, uniqTreesV3, by.x="identifier", by.y="identifier", all.x=TRUE)
+food_2012$TreeSpecies=as.factor(food_2012$TreeSpecies)
+food_2012 = food_2012[,c("StateRouteStop.x", "Station.x", "TrapType.x", "Visit3", "TreeSpecies")]
+names(food_2012) = c("StateRouteStop", "Station", "TrapType", "Visit3", "TreeSpecies")
 
 #mixed models for 2012
+mix_2012 = lmer(Visit3 ~ TrapType + (1 | TreeSpecies) + (1 | StateRouteStop), food_2012)
+mix_2012_2 =
 
 
 
