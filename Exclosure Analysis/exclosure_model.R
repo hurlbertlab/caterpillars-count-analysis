715e8bafe44f34f13d77083e8ba5ce6a1a7f7085
Tara McKinnon <tmckinno@live.unc.edu>
2016-09-19 12:46:22 -0400

    minor changes to name of dataframes being sourced

--- Exclosure Analysis/exclosure_model.R
+++ Exclosure Analysis/exclosure_model.R
@@ -6,9 +6,9 @@ source("Exclosure Analysis/ExclosureAnalysis_Summer2016.R")
 source("~/Desktop/insect-exclosure/data_analysis.R")
 
 #Figure out which probability distribution fits data -None of the following are very good (all for normal distributions)
-#food_time$Visit3.1 <- food_time$Visit3 + .1
-#qqp(food_time$Visit3.1, "norm")
-#qqp(food_time$Visit3.1, "lnorm")
+food_time$Visit3.1 <- food_time$Visit3 + .1
+qqp(food_time$Visit3.1, "norm")
+qqp(food_time$Visit3.1, "lnorm")
 
 #data shaping
 food = food_time1
@@ -30,7 +30,7 @@ mix_mod_food = lmer(food_sum ~ TrapType + (1 | surveyTrees), food)
 mix_mod_caterpillar = lmer(caterpillar_sum ~ TrapType + (1 | surveyTrees), caterpillar)
 
 #data shaping for 2012
-food_new = food_time
+food_new = food_time_12
 food_new$identifier = paste0(food_new$StateRouteStop, food_new$Station, food_new$TrapType) 
 
 #merge tree species data with density data
@@ -44,7 +44,7 @@ names(food_2012) = c("StateRouteStop", "Station", "TrapType", "Visit3", "TreeSpe
 
 #mixed models for 2012
 mix_2012 = lmer(Visit3 ~ TrapType + (1 | TreeSpecies) + (1 | StateRouteStop), food_2012)
-mix_2012_2 =
+mix_2012_2 = lmer(Visit3 ~ TrapType + (1 | TreeSpecies) + ) #StateRouteStop is really a stand in for elevation, precipitation, temperature- get these?
 
 
 
