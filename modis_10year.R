427edbc8c100e9f93f9faceaae7644fe1708dbed
hayeste <hayeste@live.unc.edu>
2016-09-22 10:46:05 -0400

    adjusted to include Hubbard Brook

--- modis_10year.R
+++ modis_10year.R
@@ -30,10 +30,10 @@ if (0) {
 for (year in 1:length(multyears)) {
 
   # Format MODIS data
-  modis = data.frame(lat = c(35.898645, 35.809674), long = c(-79.031469, -78.716546))
+  modis = data.frame(lat = c(35.898645, 35.809674, 43.942407), long = c(-79.031469, -78.716546, -71.710066))
   modis$start.date = rep(multyears[[year]], nrow(modis)) #not sure if these dates are formatted correctly
   modis$end.date = rep(multyears[[year]], nrow(modis))
-  modis$ID = c(1,2)
+  modis$ID = c(1,2,3)
 
   # Set working directory to year file the data will be downloaded in
   setwd(file.path('C:','git','caterpillars-count-analysis','modis-and-temp',multyears[[year]], sep = '/'))
@@ -51,7 +51,8 @@ for (year in 1:length(multyears)) {
 multyears <- 2007:2016
 
 samp.dataframe = data.frame(prgreenup.half = numeric(), bggreenup.half = numeric(),
-                            prgreenup.log = numeric(), bggreenup.log = numeric())
+                            hbgreenup.half = numeric(), prgreenup.log = numeric(), 
+                            bggreenup.log = numeric(), hbgreenup.log = numeric())
 
 # Set plot grid for EVI plots
 par(mfrow = c(5, 2), mar = c(2, 2, 1, 1))
@@ -64,19 +65,27 @@ for (year in 1:length(multyears)) {
   # Read in MODIS data
   bgmodis <- read.csv(list.files(pattern = ".asc")[1], header = FALSE, as.is = TRUE)
   prmodis <- read.csv(list.files(pattern = ".asc")[2], header = FALSE, as.is = TRUE)
+  hbmodis <- read.csv(list.files(pattern = ".asc")[3], header = FALSE, as.is = TRUE)
   bgmodis$julianday <- as.numeric(substring(bgmodis$V8, 6,8))
-  #bgmodis$date <- strptime(bgmodis$julianday, "%m/%d/%Y")
   prmodis$julianday <- as.numeric(substring(prmodis$V8, 6,8))
+  hbmodis$julianday <- as.numeric(substring(hbmodis$V8, 6,8))
 
   # Calculating average EVI across area (not taking into account pixel reliability)
+  # Hubbard Brook:
+  temphbevi = hbmodis[grep("EVI", hbmodis$V6),]
+  hbevi <- temphbevi[11:91]
+  hbmean1 <- apply(hbevi, 1, mean)
+  hbmean2 <- hbmean1 / 10000
+  hbmean <- data.frame(julianday = temphbevi$julianday, EVImean = hbmean2)
+  plot(hbmean$julianday, hbmean$EVImean, col = 'green3', type = 'l', 
+       xlab = "Julian Day", ylab = "Mean EVI")  
   # Botanical Garden:
   tempbgevi = bgmodis[grep("EVI", bgmodis$V6),]
   bgevi <- tempbgevi[11:91]
   bgmean1 <- apply(bgevi, 1, mean)
   bgmean2 <- bgmean1 / 10000
   bgmean <- data.frame(julianday = tempbgevi$julianday, EVImean = bgmean2)
-  plot(bgmean$julianday, bgmean$EVImean, 
-       xlab = "Julian Day", ylab = "Mean EVI", col = 'blue', type = 'l')
+  points(bgmean$julianday, bgmean$EVImean, col = 'blue', type = 'l')
   # Prairie Ridge:
   tempprevi = prmodis[grep("EVI", prmodis$V6),]
   previ <- tempprevi[11:91]
@@ -84,6 +93,7 @@ for (year in 1:length(multyears)) {
   prmean2 <- prmean1 / 10000
   prmean <- data.frame(julianday = tempprevi$julianday, EVImean = prmean2)
   points(prmean$julianday, prmean$EVImean, col = 'red', type = 'l')
+
 
   
 
@@ -112,13 +122,18 @@ for (year in 1:length(multyears)) {
   #points(bgmean$julianday, bgmean$bgEVIlog, col = 'blue', lwd = 3, 
   #       lty = 'dashed', type = 'l')
 
+  # Hubbard Brook
+  subhbmean = hbmean[hbmean$julianday %in% 1:200,]
+  subhbmean$EVIdis = subhbmean$EVImean - min(subhbmean$EVImean)+.01
+  hblog = nls(EVIdis ~ SSlogis(julianday, Asym, xmid, scal), data = subhbmean)
+  #par(mar=c(5, 4, 4, 4) + 0.1)
+  #plot(hbmean$julianday, hbmean$EVImean, xlab = "Julian Day", ylab = "Mean EVI",
+  #     col = 'blue', type = 'l', lwd = 3)
+  subhbmean$hbEVIlog = predict(hblog)+min(subhbmean$EVImean)-.01
+  #points(hbmean$julianday, hbmean$hbEVIlog, col = 'blue', lwd = 3, 
+  #       lty = 'dashed', type = 'l') 
 
-  #plot(prmean$julianday, predict(prlog)+min(prmean$EVImean)-.01, col = 'red', lwd = 3, 
-  #     lty = 'dashed', type = 'l', ylim = c(0.15, 0.53), xlab = "Julian Day", ylab = "Mean EVI")
-  #points(bgmean$julianday, predict(bglog)+min(bgmean$EVImean)-.01, col = 'blue', lwd = 3, 
-  #       lty = 'dashed', type = 'l')
-  #legend("topleft", c('BG mean EVI', 'PR mean EVI'), lwd = c(3,3), lty = c(2,2), col = c('blue', 'red'))
-  
+
   # Extract greenup from logistic fit
   
   #summary(prlog)
@@ -127,6 +142,8 @@ for (year in 1:length(multyears)) {
   #summary(bglog)
   bggreenup.log <- summary(bglog)$coefficients["xmid","Estimate"]
   
+  #summary(hblog)
+  hbgreenup.log <- summary(hblog)$coefficients["xmid","Estimate"]  
   
   # Extract greenup by finding the day the EVI is half its maximum:
   
@@ -149,25 +166,38 @@ for (year in 1:length(multyears)) {
   jd2 = subprmean$julianday[subprmean$inflectdiff == head(subprmean$inflectdiff[subprmean$inflectdiff >= 0], 1)]
   prgreenup.half = jd1 + ((yhat-y1)/(y2-y1))*(jd2-jd1)
   #figure out how to do this with regular prmean?
+  
+  # Hubbard Brook
+  subhbmean$inflectdiff = subhbmean$EVImean - (0.5*(max(subhbmean$EVImean)-min(subhbmean$EVImean)) + min(subhbmean$EVImean))
+  yhat = 0.5*(max(subhbmean$EVImean)-min(subhbmean$EVImean)) + min(subhbmean$EVImean)
+  y1 = subhbmean$EVImean[subhbmean$inflectdiff == tail(subhbmean$inflectdiff[subhbmean$inflectdiff <= 0], 1)]
+  y2 = subhbmean$EVImean[subhbmean$inflectdiff == head(subhbmean$inflectdiff[subhbmean$inflectdiff >= 0], 1)]
+  jd1 = subhbmean$julianday[subhbmean$inflectdiff == tail(subhbmean$inflectdiff[subhbmean$inflectdiff <= 0], 1)]
+  jd2 = subhbmean$julianday[subhbmean$inflectdiff == head(subhbmean$inflectdiff[subhbmean$inflectdiff >= 0], 1)]
+  hbgreenup.half = jd1 + ((yhat-y1)/(y2-y1))*(jd2-jd1)
+  #figure out how to do this with regular hbmean?
 
-  temp.dataframe = data.frame(prgreenup.half, bggreenup.half, prgreenup.log, bggreenup.log)
+  temp.dataframe = data.frame(prgreenup.half, bggreenup.half, hbgreenup.half, 
+                              prgreenup.log, bggreenup.log, hbgreenup.log)
 
   samp.dataframe = rbind(samp.dataframe, temp.dataframe)
 } # end for loop
 
 greenup <- samp.dataframe
-greenup$year <- c(2006:2015)
+greenup$year <- c(2007:2016)
 
 # Plotting
-par(mar = c(2,2,3,2), mfrow = c(1,1), oma = c(0,0,0,0))
-plot(greenup$year, greenup$prgreenup.log, col = 'red', type = 'l', ylim = c(75,120),
+par(mar = c(2,2,3,2), mfrow = c(1,1), oma = c(2,2,2,2))
+plot(greenup$year, greenup$prgreenup.log, col = 'red', type = 'l', ylim = c(75,160),
      xlab = 'Year', ylab = "Julian day of greenup", lwd = 2)
 points(greenup$year, greenup$bggreenup.log, col = 'blue', type = 'l', lwd = 2)
+points(greenup$year, greenup$hbgreenup.log, col = 'green3', type = 'l', lwd = 2)
 points(greenup$year, greenup$prgreenup.half, col = 'red', type = 'l', lwd = 2, lty = 2)
 points(greenup$year, greenup$bggreenup.half, col = 'blue', type = 'l', lwd = 2, lty = 2)
-legend("topleft", c('PR logistic', 'BG logistic', 'PR half max', 'BG half max'), lwd = 2, 
-       lty = c(1,1,2,2), col = c('red', 'blue', 'red', 'blue')) 
-title('Greenup 2006-2015', line = 1)
+points(greenup$year, greenup$hbgreenup.half, col = 'green3', type = 'l', lwd = 2, lty = 2)
+legend("topleft", c('PR logistic', 'BG logistic', 'HB logistic', 'PR half max', 'BG half max', 'HB half max'), lwd = 2, 
+       lty = c(1,1,1,2,2,2), col = c('red', 'blue', 'green3', 'red', 'blue', 'green3')) 
+title('Greenup 2007-2016', line = 1)
 
 
 
