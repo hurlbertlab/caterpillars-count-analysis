d270f3e97a045e485755612966200ec1bef644e8
UNC Support <tmckinno@live.unc.edu>
2016-09-26 12:59:47 -0400

    read in surveyTree data and changed 2014/2015 survey trees to reflect trees species that have changed

--- tree_species_model.R
+++ tree_species_model.R
@@ -1,6 +1,9 @@
 #Tree species model
 source("data_cleaning.R")
 
+#read in data
+all_surveyTrees <- read.csv("data/tbl_surveyTrees.csv", header=T)
+
 #Create 1 dataset with all data from PR/NCBG 2015 & 2016
 lab.triangle = rbind(labdata.pr, labdata.bg)
 
@@ -20,16 +23,37 @@ vis_biomass = dplyr::summarise(vis_grouped, sum(biomass))
 vis_biomass$identifier = paste0(vis_biomass$site, vis_biomass$circle, vis_biomass$survey, vis_biomass$date)
 names(vis_biomass) = c("site", "circle", "survey", "date", "sum_biomass", "identifier")
 
-#merge to add tree species data to summary data
-vis_plants = unique(vis[, c("identifier", "plantSp")]) #there are 16 observations that have multiple plant species for the same surveys- need to go through and find these and correct
-#vis_plants1 = data.frame(table(vis_plants))
-#vis_plants2 = vis_plants1[vis_plants1$Freq> 0,]
-vis_count_final= merge(vis_count, vis_plants, by.x = "identifier", by.y = "identifier", all.x = T) #this has larger dimensions than vis_count because of extra plant species
-vis_biomass_final= merge(vis_biomass, vis_plants, by.x = "identifier", by.y = "identifier", all.x = T)
+#add year column to summarized arth densities
+vis_year = unique(vis[, c("identifier", "year")])
+vis_count1 = merge(vis_count, vis_year, by.x = "identifier", by.y = "identifier")
+vis_biomass1 = merge(vis_biomass, vis_year, by.x="identifier", by.y="identifier")
 
-#create list of unique surveys
-#uniq_surv = unique(labdata.triangle[, c("site", "circle", "survey", "date")])
-#uniq_surv$identifier = paste0(uniq_surv$site, uniq_surv$date, uniq_surv$circle, uniq_surv$survey)
+#Get list of tree species for each year for NCBG & PR
+all_surveyTrees_triangle = all_surveyTrees[all_surveyTrees$siteID %in% c("117", "8892356"),]
+trees_2016 = all_surveyTrees_triangle
+trees_2016$year = "2016"
+trees_2015 = all_surveyTrees_triangle
+trees_2015$year = "2015"
+
+#Make changes to 2015 tree species to reflect species that were changed in 2016
+trees_2015$plantSpecies = as.character(trees_2015$plantSpecies)
+trees_2015[11, 4] = "Devil's walkingstick"  #changed 117 3A
+trees_2015[34, 4] = "Tuliptree" #changed 117 7D
+trees_2015[77, 4] = "Mapleleaf viburnum" #changed 8892356 4B
+trees_2015[95, 4] = "Sugar maple" #changed 8892356 7E
+trees_2015$plantSpecies = as.factor(trees_2015$plantSpecies)
+
+#add trees 2014
+trees_2014 = trees_2015
+trees_2014$year = "2014"
+
+#create one dataframe with tree species for 2015 & 2016
+trees_both = rbind(trees_2015, trees_2016)
+
+#merge to add tree species data to summary data
+vis_count1$loc_ID = paste0("site", "circle", "survey", "year")
+vis_count_final = merge(vis_count1, trees_both, by.x = "identifier", by.y = "identifier", all.x = T) #have to create identifier that works for both of these dfs 
+vis_biomass_final = merge(vis_biomass1, trees_both, by.x = "identifier", by.y = "identifier", all.x = T)
 
 #Modeling
 lm.count = lm(sum_count ~ plantSp, data=vis_count_final) #so many different capitalizations of plant species, need to merge with plant species master lists for PR and NCBG
