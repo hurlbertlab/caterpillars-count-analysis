aa634f6bb6f2d276b4f19f48db921d00d5aea2d9
hayeste <hayeste@live.unc.edu>
2016-10-27 12:08:11 -0400

    starting gdd and greenup deviation plots

--- compilation.R
+++ compilation.R
@@ -11,6 +11,28 @@ setwd('c:/git/caterpillars-count-analysis')
 
 greenupgdd <- merge(gddyear, greenup, by = 'year')
 
+# Calculate deviation from mean of greenup and gdd at each site
+greenupgdd$pr.gdd.dev <- greenupgdd$pr.gdd - mean(greenupgdd$pr.gdd)
+greenupgdd$bg.gdd.dev <- greenupgdd$bg.gdd - mean(greenupgdd$bg.gdd)
+greenupgdd$prgreenup.dev <- greenupgdd$prgreenup.log - mean(greenupgdd$prgreenup.log)
+greenupgdd$bggreenup.dev <- greenupgdd$bggreenup.log - mean(greenupgdd$bggreenup.log)
+
+# Plotting deviations
+par(mar = c(4,4,2,2))
+# GDD and greenup
+plot(greenupgdd$pr.gdd.dev, greenupgdd$prgreenup.dev, col = 'white',
+     xlab = 'GDD JD deviation from mean', ylab = 'Greenup JD deviation from mean')
+abline(h = 0, lty = 2)
+abline(v = 0, lty = 2)
+text(greenupgdd$pr.gdd.dev, greenupgdd$prgreenup.dev, greenupgdd$year, col = 'red')
+text(greenupgdd$bg.gdd.dev, greenupgdd$bggreenup.dev, greenupgdd$year, col = 'blue')
+# Caterpillars and greenup
+
+# Caterpillars and GDD
+
+
+# ANOVAs
+
 prmod1 <- lm(prgreenup.log ~ pr.gdd, data = greenupgdd)
 summary(prmod1) # significant
 prmod2 <- lm(prgreenup.half ~ pr.gdd, data = greenupgdd)
@@ -32,6 +54,14 @@ summary(hbmod2)
 
 # None of the others significant
 # [GDD not a good predictor of greenup??]
+
+# Interesting plots (from meeting on 10/26/2016)
+plot(greenupgdd$pr.gdd, greenupgdd$prgreenup.log)
+text(greenupgdd$pr.gdd, greenupgdd$prgreenup.log, greenupgdd$year)
+plot(greenupgdd$bg.gdd, greenupgdd$bggreenup.log)
+text(greenupgdd$bg.gdd, greenupgdd$bggreenup.log, greenupgdd$year)
+plot(greenupgdd$bg.gdd, greenupgdd$pr.gdd)
+plot(greenupgdd$prgreenup.log, greenupgdd$bggreenup.log)
 
 
 
