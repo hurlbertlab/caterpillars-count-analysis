b3e5eb2ba65e791209c799362a69978add3a95ef
hayeste <hayeste@live.unc.edu>
2016-10-28 13:38:08 -0400

    working on cat comparisons

--- compilation.R
+++ compilation.R
@@ -4,6 +4,8 @@ source('C:/git/caterpillars-count-analysis/prism_10year.R')
 
 source('C:/git/caterpillars-count-analysis/modis_10year.R')
 
+source('C:/git/caterpillars-count-analysis/extensive_plotting.R')
+
 setwd('c:/git/caterpillars-count-analysis')
 
 # Is greenup affected by GDD?
@@ -17,11 +19,17 @@ greenupgdd$bg.gdd.dev <- greenupgdd$bg.gdd - mean(greenupgdd$bg.gdd)
 greenupgdd$prgreenup.dev <- greenupgdd$prgreenup.log - mean(greenupgdd$prgreenup.log)
 greenupgdd$bggreenup.dev <- greenupgdd$bggreenup.log - mean(greenupgdd$bggreenup.log)
 
+# Calculate max cat for 2015 and 2016 (visual, beat sheet)
+# Still unsure about what to use as peak to find JD
+# Just largest number before a certain julian day like 180?
+#greenupgdd$prlepl15 <- c(rep(0,8), PR.LEPL15[max(PR.LEPL15[PR.LEPL15$julianday %in% c(80:180),]$meanDensity),]$julianday)
+
 # Plotting deviations
 par(mar = c(4,4,2,2))
 # GDD and greenup
 plot(greenupgdd$pr.gdd.dev, greenupgdd$prgreenup.dev, col = 'white',
      xlab = 'GDD JD deviation from mean', ylab = 'Greenup JD deviation from mean')
+#abline(1,0)
 abline(h = 0, lty = 2)
 abline(v = 0, lty = 2)
 text(greenupgdd$pr.gdd.dev, greenupgdd$prgreenup.dev, greenupgdd$year, col = 'red')
@@ -29,6 +37,10 @@ text(greenupgdd$bg.gdd.dev, greenupgdd$bggreenup.dev, greenupgdd$year, col = 'bl
 # Caterpillars and greenup
 
 # Caterpillars and GDD
+
+# Birds and greenup
+
+# Birds and GDD
 
 
 # ANOVAs
