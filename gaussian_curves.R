ef7e6b61da829e2f7d01c90b32c4ae3368479d39
hayeste <hayeste@live.unc.edu>
2016-11-18 11:44:34 -0500

    first commit attempts to fit gaussian curves to density data

---
+++ gaussian_curves.R
@@ -0,0 +1,87 @@
+
+
+# Make a fake Gaussian to test (?)
+set.seed(3000)
+xseq<-seq(-4,4,.01)
+densities<-dnorm(xseq, 0,1)
+cumulative<-pnorm(xseq, 0, 1)
+randomdeviates<-rnorm(100,0,1)
+
+par(mar=c(3,4,4,2), mfrow = c(1,1))
+
+plot(xseq, densities, col="darkgreen",xlab="", ylab="Density", type="l",lwd=2, cex=2, main="Standard Normal", cex.axis=.8)
+
+#####################################################################
+# Fitting Gaussians
+
+fitG = function(x, y, mu, sig, scale, ...){
+  f = function(p){
+    d = p[3] * dnorm(x, mean = p[1], sd = p[2])
+    sum((d - y) ^ 2)
+  }
+  optim(c(mu, sig, scale), f)
+}
+###################################################################
+
+multorders <- c('LEPL', 'ORTH', 'ARAN','COLE', 'HEMI')
+
+
+
+
+
+#----Test 1----
+
+# Make plot/data of what you will fit normal curve to
+prlepl15.weekly = meanDensityByWeek(amsurvey.pr[amsurvey.pr$surveyType == 'Visual',], 
+                                    ordersToInclude = "LEPL", inputYear = 2015, inputSite = 117, plot = T, 
+                                    plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
+                                    xlim = c(18,32), ylim = c(0,.2), ylab = "Mean density", main = 'PR LEPL by week')
+
+# Fit a normal curve using least squares
+gfit = fitG(prlepl15.weekly$week, prlepl15.weekly$meanDensity, weighted.mean(prlepl15.weekly$week, prlepl15.weekly$meanDensity),
+            5, 3000, control = list(maxit = 10000), method="L-BFGS-B", lower=c(0,0,0,0,0,0))
+# "The warning messages produced are probably due to the sd parameter going negative." -Stack Overflow
+
+# Curve parameters
+p = gfit$par
+r2 = cor(prlepl15.weekly$week, p[3]*dnorm(prlepl15.weekly$week, p[1], p[2]))^2
+totalDensity = sum(prlepl15.weekly$meanDensity)
+lines(18:32, p[3]*dnorm(18:32, p[1], p[2]), col = 'blue') # make sure it appears on the right plot
+
+
+#----Test 2----
+
+# Make plot/data of what you will fit normal curve to
+prlepl15.daily = meanDensityByDay(amsurvey.pr[amsurvey.pr$surveyType == 'Visual',], 
+                                  ordersToInclude = "LEPL", inputYear = 2015, inputSite = 117, plot = T, 
+                                  plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
+                                  xlim = c(130,210), ylim = c(0,.2), ylab = "Mean density", main = 'PR LEPL by day')
+
+# Fit a normal curve using least squares
+gfit2 = fitG(prlepl15.daily$julianday, prlepl15.daily$meanDensity, weighted.mean(prlepl15.daily$julianday, prlepl15.daily$meanDensity),
+             5, 300, control = list(maxit = 10000), method="L-BFGS-B", lower=c(0,0,0,0,0,0))
+
+# Curve parameters
+p2 = gfit2$par
+r2.2 = cor(prlepl15.daily$julianday, p[3]*dnorm(prlepl15.daily$julianday, p[1], p[2]))^2
+totalDensity = sum(prlepl15.daily$meanDensity)
+lines(130:210, p[3]*dnorm(130:210, p[1], p[2]), col = 'blue') # make sure it appears on the right plot
+
+#----Test 3----
+
+# Make plot/data of what you will fit normal curve to
+prmult15.weekly = meanDensityByWeek(amsurvey.pr[amsurvey.pr$surveyType == 'Visual',], 
+                                    ordersToInclude = multorders, inputYear = 2015, inputSite = 117, plot = T, 
+                                    plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
+                                    xlim = c(18,32), ylim = c(.1,.7), ylab = "Mean density", main = 'PR mult by week')
+
+# Fit a normal curve using least squares
+gfit2 = fitG(prmult15.weekly$julianday, prmult15.weekly$meanDensity, weighted.mean(prmult15.weekly$week, prmult15.weekly$meanDensity),
+             5, 3000, control = list(maxit = 10000), method="L-BFGS-B", lower=c(0,0,0,0,0,0))
+
+# Curve parameters
+p2 = gfit2$par
+r2.2 = cor(prmult15.weekly$week, p[3]*dnorm(prmult15.weekly$week, p[1], p[2]))^2
+totalDensity = sum(prmult15.weekly$meanDensity)
+lines(130:210, p[3]*dnorm(130:210, p[1], p[2]), col = 'blue') # make sure it appears on the right plot
+
