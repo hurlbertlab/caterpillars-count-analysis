09fb0a24dd1a8ab2c9dfd1f14f5f086b9beca254
hayeste <hayeste@live.unc.edu>
2016-11-21 14:19:04 -0500

    changing scaling factors

--- gaussian_curves.R
+++ gaussian_curves.R
@@ -21,12 +21,12 @@ fitG = function(x, y, mu, sig, scale, ...){
   }
   optim(c(mu, sig, scale), f)
 }
+
+# mu = mean
+# sig = standard deviation
 ###################################################################
 
 multorders <- c('LEPL', 'ORTH', 'ARAN','COLE', 'HEMI')
-
-
-
 
 
 #----Test 1----
@@ -39,8 +39,7 @@ prlepl15.weekly = meanDensityByWeek(amsurvey.pr[amsurvey.pr$surveyType == 'Visua
 
 # Fit a normal curve using least squares
 gfit = fitG(prlepl15.weekly$week, prlepl15.weekly$meanDensity, weighted.mean(prlepl15.weekly$week, prlepl15.weekly$meanDensity),
-            5, 3000, control = list(maxit = 10000), method="L-BFGS-B", lower=c(0,0,0,0,0,0))
-# "The warning messages produced are probably due to the sd parameter going negative." -Stack Overflow
+            2, 3.5, control = list(maxit = 10000), method="L-BFGS-B", lower=c(0,0,0,0,0,0))
 
 # Curve parameters
 p = gfit$par
@@ -59,13 +58,13 @@ prlepl15.daily = meanDensityByDay(amsurvey.pr[amsurvey.pr$surveyType == 'Visual'
 
 # Fit a normal curve using least squares
 gfit2 = fitG(prlepl15.daily$julianday, prlepl15.daily$meanDensity, weighted.mean(prlepl15.daily$julianday, prlepl15.daily$meanDensity),
-             5, 300, control = list(maxit = 10000), method="L-BFGS-B", lower=c(0,0,0,0,0,0))
+             20, 4, control = list(maxit = 10000), method="L-BFGS-B", lower=c(0,0,0,0,0,0))
 
 # Curve parameters
 p2 = gfit2$par
-r2.2 = cor(prlepl15.daily$julianday, p[3]*dnorm(prlepl15.daily$julianday, p[1], p[2]))^2
+r2.2 = cor(prlepl15.daily$julianday, p2[3]*dnorm(prlepl15.daily$julianday, p2[1], p2[2]))^2
 totalDensity = sum(prlepl15.daily$meanDensity)
-lines(130:210, p[3]*dnorm(130:210, p[1], p[2]), col = 'blue') # make sure it appears on the right plot
+lines(130:210, p2[3]*dnorm(130:210, p2[1], p2[2]), col = 'blue') # make sure it appears on the right plot
 
 #----Test 3----
 
@@ -76,12 +75,12 @@ prmult15.weekly = meanDensityByWeek(amsurvey.pr[amsurvey.pr$surveyType == 'Visua
                                     xlim = c(18,32), ylim = c(.1,.7), ylab = "Mean density", main = 'PR mult by week')
 
 # Fit a normal curve using least squares
-gfit2 = fitG(prmult15.weekly$julianday, prmult15.weekly$meanDensity, weighted.mean(prmult15.weekly$week, prmult15.weekly$meanDensity),
-             5, 3000, control = list(maxit = 10000), method="L-BFGS-B", lower=c(0,0,0,0,0,0))
+gfit3 = fitG(prmult15.weekly$week, prmult15.weekly$meanDensity, weighted.mean(prmult15.weekly$week, prmult15.weekly$meanDensity),
+             4, 10, control = list(maxit = 10000), method="L-BFGS-B", lower=c(0,0,0,0,0,0))
 
 # Curve parameters
-p2 = gfit2$par
-r2.2 = cor(prmult15.weekly$week, p[3]*dnorm(prmult15.weekly$week, p[1], p[2]))^2
+p3 = gfit3$par
+r2.3 = cor(prmult15.weekly$week, p3[3]*dnorm(prmult15.weekly$week, p3[1], p3[2]))^2
 totalDensity = sum(prmult15.weekly$meanDensity)
-lines(130:210, p[3]*dnorm(130:210, p[1], p[2]), col = 'blue') # make sure it appears on the right plot
+lines(18:32, p3[3]*dnorm(18:32, p3[1], p3[2]), col = 'blue') # make sure it appears on the right plot
 
