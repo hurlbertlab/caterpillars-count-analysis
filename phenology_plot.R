8340714cf45e98ef7d311a3471d47bb8235019d7
hayeste <hayeste@live.unc.edu>
2016-11-30 15:11:22 -0500

    six-panel figure 3

--- phenology_plot.R
+++ phenology_plot.R
@@ -1,49 +1,76 @@
 # Phenology plot for paper
 # Need volunteer beat sheet data from 2016
 
-par(mfrow = c(2,2))
+setwd('c:/git/caterpillars-count-analysis')
+source("data_cleaning.r")
+
+par(mfrow = c(3,2), mar = c(3,4,3,2))
 multorders <- c('LEPL', 'ORTH', 'ARAN','COLE', 'HEMI')
 
+
 # First panel
-PR.LEPL15.sci = meanDensityByDay(amsurvey.pr[amsurvey.pr$surveyType == 'Visual',], 
+PR.LEPL15.sci = meanDensityByDay(amsurvey.pr[amsurvey.pr$surveyType == 'Visual' & amsurvey.pr$julianday %in% c(134:204),], 
                                  ordersToInclude = "LEPL", inputYear = 2015, inputSite = 117, plot = T, 
                                  plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
-                                 xlim = c(130,208), ylim = c(0,.33), ylab = "Mean density of caterpillars", main = '2015 Visual')
-PR.LEPL15.cs = meanDensityByDay(volunteer.pr,  
+                                 xlim = c(130,207), ylim = c(0,.36), ylab = "Mean density of caterpillars", main = '2015 Visual')
+PR.LEPL15.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:204),],  
                                 ordersToInclude = "LEPL", inputYear = 2015, inputSite = 117, plot = T, 
                                 plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2, lty = 2)
 legend("topleft", c('trained scientists', 'citizen scientists'), lwd = 2, lty = c(1, 2))
 
 
 # Second panel
-PR.LEPL16.sci = meanDensityByDay(beatsheet.pr[beatsheet.pr$surveyType == 'Beat_Sheet',], 
+PR.LEPL16.sci = meanDensityByDay(beatsheet.pr[beatsheet.pr$surveyType == 'Beat_Sheet' & beatsheet.pr$julianday %in% c(134:204),], 
                                  ordersToInclude = "LEPL", inputYear = 2016, inputSite = 117, plot = T, 
                                  plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
-                                 xlim = c(130,208), ylim = c(0,.33), ylab = "Mean density of caterpillars", main = '2016 Beat Sheet')
-#PR.LEPL16.cs = meanDensityByDay(volunteer.pr,  
-#                             ordersToInclude = "LEPL", inputYear = 2016, inputSite = 117, plot = T, 
-#                             plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2)
+                                 xlim = c(130,207), ylim = c(0,.2), ylab = "", main = '2016 Beat Sheet')
+PR.LEPL16.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:204),],  
+                             ordersToInclude = "LEPL", inputYear = 2016, inputSite = 117, plot = T, 
+                             plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2, lty = 2)
 #legend("topleft", c('trained scientists', 'citizen scientists'), lwd = 2, lty = c(1, 2))
 
+
 # Third panel
-PR.BIRD15.sci = meanDensityByDay(amsurvey.pr[amsurvey.pr$surveyType == 'Visual',], 
-                                 ordersToInclude = multorders, inputYear = 2015, inputSite = 117, plot = T, 
+PR.LEPL15.sci = meanDensityByDay(amsurvey.pr[amsurvey.pr$surveyType == 'Visual' & amsurvey.pr$julianday %in% c(134:204),], 
+                                 ordersToInclude = "ORTH", inputYear = 2015, inputSite = 117, plot = T, 
                                  plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
-                                 xlim = c(130,208), ylim = c(0,1.5), ylab = "Mean density of bird food", main = '')
-PR.BIRD15.cs = meanDensityByDay(volunteer.pr,  
-                                ordersToInclude = multorders, inputYear = 2015, inputSite = 117, plot = T, 
+                                 xlim = c(130,207), ylim = c(0,.25), ylab = "Mean density of orthopterans", main = '')
+PR.LEPL15.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:204),],  
+                                ordersToInclude = "ORTH", inputYear = 2015, inputSite = 117, plot = T, 
                                 plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2, lty = 2)
 #legend("topleft", c('trained scientists', 'citizen scientists'), lwd = 2, lty = c(1, 2))
 
 
 # Fourth panel
-PR.BIRDL16.sci = meanDensityByDay(beatsheet.pr[beatsheet.pr$surveyType == 'Beat_Sheet',], 
+PR.LEPL16.sci = meanDensityByDay(beatsheet.pr[beatsheet.pr$surveyType == 'Beat_Sheet' & beatsheet.pr$julianday %in% c(134:204),], 
+                                 ordersToInclude = "ORTH", inputYear = 2016, inputSite = 117, plot = T, 
+                                 plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
+                                 xlim = c(130,207), ylim = c(0,.7), ylab = "", main = '')
+PR.LEPL16.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:204),],  
+                                ordersToInclude = "ORTH", inputYear = 2016, inputSite = 117, plot = T, 
+                                plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2, lty = 2)
+#legend("topleft", c('trained scientists', 'citizen scientists'), lwd = 2, lty = c(1, 2))
+
+
+# Fifth panel
+PR.BIRD15.sci = meanDensityByDay(amsurvey.pr[amsurvey.pr$surveyType == 'Visual' & amsurvey.pr$julianday %in% c(134:204),], 
+                                 ordersToInclude = multorders, inputYear = 2015, inputSite = 117, plot = T, 
+                                 plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
+                                 xlim = c(130,207), ylim = c(0,1.5), ylab = "Mean density of bird food", main = '')
+PR.BIRD15.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:204),],  
+                                ordersToInclude = multorders, inputYear = 2015, inputSite = 117, plot = T, 
+                                plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2, lty = 2)
+#legend("topleft", c('trained scientists', 'citizen scientists'), lwd = 2, lty = c(1, 2))
+
+
+# Sixth panel
+PR.BIRDL16.sci = meanDensityByDay(beatsheet.pr[beatsheet.pr$surveyType == 'Beat_Sheet' & beatsheet.pr$julianday %in% c(134:204),], 
                                   ordersToInclude = multorders, inputYear = 2016, inputSite = 117, plot = T, 
                                   plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
-                                  xlim = c(130,208), ylim = c(0,1.5), ylab = "Mean density of bird food", main = '')
-#PR.BIRD16.cs = meanDensityByDay(volunteer.pr,  
-#                                ordersToInclude = multorders, inputYear = 2016, inputSite = 117, plot = T, 
-#                                plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2)
+                                  xlim = c(130,207), ylim = c(0,1.2), ylab = "", main = '')
+PR.BIRD16.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:204),],  
+                                ordersToInclude = multorders, inputYear = 2016, inputSite = 117, plot = T, 
+                                plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2, lty = 2)
 #legend("topleft", c('trained scientists', 'citizen scientists'), lwd = 2, lty = c(1, 2))
 
 
