5a822decf957964a201ef3a8989c0e4ad483f0b1
hayeste <hayeste@live.unc.edu>
2017-01-11 13:32:06 -0500

    adjustments to labeling

--- phenology_plot.R
+++ phenology_plot.R
@@ -4,15 +4,16 @@
 setwd('c:/git/caterpillars-count-analysis')
 source("data_cleaning.r")
 
-par(mfrow = c(3,2), mar = c(3,4,3,2))
+par(mfrow = c(3,2), mar = c(3,4,3,2), oma = c(3,3,2,2))
 multorders <- c('LEPL', 'ORTH', 'ARAN','COLE', 'HEMI')
 
+#----PHENOLOGY PLOT MEAN DENSITY----
 
 # First panel
 PR.LEPL15.sci = meanDensityByDay(amsurvey.pr[amsurvey.pr$surveyType == 'Visual' & amsurvey.pr$julianday %in% c(134:204),], 
                                  ordersToInclude = "LEPL", inputYear = 2015, inputSite = 117, plot = T, 
                                  plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
-                                 xlim = c(130,207), ylim = c(0,.36), ylab = "Mean density of caterpillars", main = '2015 Visual')
+                                 xlim = c(130,207), ylim = c(0,.36), ylab = "Caterpillars", main = '2015 Visual')
 PR.LEPL15.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:204),],  
                                 ordersToInclude = "LEPL", inputYear = 2015, inputSite = 117, plot = T, 
                                 plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2, lty = 2)
@@ -34,7 +35,7 @@ PR.LEPL16.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:2
 PR.LEPL15.sci = meanDensityByDay(amsurvey.pr[amsurvey.pr$surveyType == 'Visual' & amsurvey.pr$julianday %in% c(134:204),], 
                                  ordersToInclude = "ORTH", inputYear = 2015, inputSite = 117, plot = T, 
                                  plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
-                                 xlim = c(130,207), ylim = c(0,.25), ylab = "Mean density of orthopterans", main = '')
+                                 xlim = c(130,207), ylim = c(0,.25), ylab = "Orthopterans", main = '')
 PR.LEPL15.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:204),],  
                                 ordersToInclude = "ORTH", inputYear = 2015, inputSite = 117, plot = T, 
                                 plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2, lty = 2)
@@ -56,7 +57,7 @@ PR.LEPL16.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:2
 PR.BIRD15.sci = meanDensityByDay(amsurvey.pr[amsurvey.pr$surveyType == 'Visual' & amsurvey.pr$julianday %in% c(134:204),], 
                                  ordersToInclude = multorders, inputYear = 2015, inputSite = 117, plot = T, 
                                  plotVar = 'meanDensity', new = T, minLength = 5, lwd = 2,
-                                 xlim = c(130,207), ylim = c(0,1.5), ylab = "Mean density of bird food", main = '')
+                                 xlim = c(130,207), ylim = c(0,1.5), ylab = "Bird food", main = '')
 PR.BIRD15.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:204),],  
                                 ordersToInclude = multorders, inputYear = 2015, inputSite = 117, plot = T, 
                                 plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2, lty = 2)
@@ -73,7 +74,10 @@ PR.BIRD16.cs = meanDensityByDay(volunteer.pr[volunteer.pr$julianday %in% c(134:2
                                 plotVar = 'meanDensity', new = F, minLength = 5, lwd = 2, lty = 2)
 #legend("topleft", c('trained scientists', 'citizen scientists'), lwd = 2, lty = c(1, 2))
 
+mtext("Julian day", side=1, line=1, outer=TRUE)
+mtext("Mean Density", side=2, line=1, outer=TRUE)
 
+#----GAUSSIAN FITS FOR TABLE 1 PHENOLOGY----
 
 fitG = function(x, y, mu, sig, scale, ...){
   f = function(p){
@@ -276,6 +280,67 @@ r2 = cor(PR.BIRD16.cs$julianday, p[3]*dnorm(PR.BIRD16.cs$julianday, p[1], p[2]))
 totalDensity = sum(PR.BIRD16.cs$meanDensity)
 lines(130:207, p[3]*dnorm(130:207, p[1], p[2]), col = 'blue') # make sure it appears on the right plot
 
+#----PHENOLOGY SHIFT: LEPL and bird food, 2015 and 2016, mean density, both sites----
+# See compilation.R for more
+
+par(mar = c(4,4,2,2))
+# Prairie Ridge
+plot(0,bty='n',pch='',ylab='Arth JD',xlab='Greenup JD', ylim = c(160,185), xlim = c(85, 95),
+     main = 'Visual Surveys: Arth JD vs. Greenup JD')
+maxden1 <- max(PR.LEPL15vis[PR.LEPL15vis$julianday %in% c(80:185),]$meanDensity)
+points(greenupgdd$prgreenup.log[greenupgdd$year == 2015], 
+       PR.LEPL15vis$julianday[PR.LEPL15vis$meanDensity == maxden1 & PR.LEPL15vis$julianday %in% c(80:185)], 
+       pch = 21, type = 'p', col = 'red')
+maxden2 <- max(PR.BIRD15vis[PR.BIRD15vis$julianday %in% c(80:185),]$meanDensity)
+points(greenupgdd$prgreenup.log[greenupgdd$year == 2015], 
+       PR.BIRD15vis$julianday[PR.BIRD15vis$meanDensity == maxden2 & PR.BIRD15vis$julianday %in% c(80:185)], 
+       pch = 22, type = 'p', col = 'red')
+maxden3 <- max(PR.LEPL16vis[PR.LEPL16vis$julianday %in% c(80:185),]$meanDensity)
+points(greenupgdd$prgreenup.log[greenupgdd$year == 2016], 
+       PR.LEPL16vis$julianday[PR.LEPL16vis$meanDensity == maxden3 & PR.LEPL16vis$julianday %in% c(80:185)], 
+       pch = 16, type = 'p', col = 'red')
+maxden4 <- max(PR.BIRD16vis[PR.BIRD16vis$julianday %in% c(80:185),]$meanDensity)
+points(greenupgdd$prgreenup.log[greenupgdd$year == 2016], 
+       PR.BIRD16vis$julianday[PR.BIRD16vis$meanDensity == maxden4 & PR.BIRD16vis$julianday %in% c(80:185)], 
+       pch = 15, type = 'p', col = 'red')
+segments(x0 = greenupgdd$prgreenup.log[greenupgdd$year == 2015], 
+         y0 = PR.LEPL15vis$julianday[PR.LEPL15vis$meanDensity == maxden1 & PR.LEPL15vis$julianday %in% c(80:185)], 
+         x1 = greenupgdd$prgreenup.log[greenupgdd$year == 2016], 
+         y1 = PR.LEPL16vis$julianday[PR.LEPL16vis$meanDensity == maxden3 & PR.LEPL16vis$julianday %in% c(80:185)], 
+         col = 'red')
+segments(x0 = greenupgdd$prgreenup.log[greenupgdd$year == 2015], 
+         y0 = PR.BIRD15vis$julianday[PR.BIRD15vis$meanDensity == maxden2 & PR.BIRD15vis$julianday %in% c(80:185)], 
+         x1 = greenupgdd$prgreenup.log[greenupgdd$year == 2016], 
+         y1 = PR.BIRD16vis$julianday[PR.BIRD16vis$meanDensity == maxden4 & PR.BIRD16vis$julianday %in% c(80:185)], 
+         col = 'red')
+# Botanical Garden
+maxden5 <- max(BG.LEPL15vis[BG.LEPL15vis$julianday %in% c(80:185),]$meanDensity)
+points(greenupgdd$bggreenup.log[greenupgdd$year == 2015], 
+       BG.LEPL15vis$julianday[BG.LEPL15vis$meanDensity == maxden5 & BG.LEPL15vis$julianday %in% c(80:185)], 
+       pch = 21, type = 'p', col = 'blue')
+maxden6 <- max(BG.BIRD15vis[BG.BIRD15vis$julianday %in% c(80:185),]$meanDensity)
+points(greenupgdd$bggreenup.log[greenupgdd$year == 2015], 
+       BG.BIRD15vis$julianday[BG.BIRD15vis$meanDensity == maxden6 & BG.BIRD15vis$julianday %in% c(80:185)], 
+       pch = 22, type = 'p', col = 'blue')
+maxden7 <- max(BG.LEPL16vis[BG.LEPL16vis$julianday %in% c(80:185),]$meanDensity)
+points(greenupgdd$bggreenup.log[greenupgdd$year == 2016], 
+       BG.LEPL16vis$julianday[BG.LEPL16vis$meanDensity == maxden7 & BG.LEPL16vis$julianday %in% c(80:185)], 
+       pch = 16, type = 'p', col = 'blue')
+maxden8 <- max(BG.BIRD16vis[BG.BIRD16vis$julianday %in% c(80:185),]$meanDensity)
+points(greenupgdd$bggreenup.log[greenupgdd$year == 2016], 
+       BG.BIRD16vis$julianday[BG.BIRD16vis$meanDensity == maxden8 & BG.BIRD16vis$julianday %in% c(80:185)], 
+       pch = 15, type = 'p', col = 'blue')
+segments(x0 = greenupgdd$bggreenup.log[greenupgdd$year == 2015], 
+         y0 = BG.LEPL15vis$julianday[BG.LEPL15vis$meanDensity == maxden5 & BG.LEPL15vis$julianday %in% c(80:185)], 
+         x1 = greenupgdd$bggreenup.log[greenupgdd$year == 2016], 
+         y1 = BG.LEPL16vis$julianday[BG.LEPL16vis$meanDensity == maxden7 & BG.LEPL16vis$julianday %in% c(80:185)], 
+         col = 'blue')
+segments(x0 = greenupgdd$bggreenup.log[greenupgdd$year == 2015], 
+         y0 = BG.BIRD15vis$julianday[BG.BIRD15vis$meanDensity == maxden6 & BG.BIRD15vis$julianday %in% c(80:185)], 
+         x1 = greenupgdd$bggreenup.log[greenupgdd$year == 2016], 
+         y1 = BG.BIRD16vis$julianday[BG.BIRD16vis$meanDensity == maxden8 & BG.BIRD16vis$julianday %in% c(80:185)], 
+         col = 'blue')
+#legend('topright', c('2015 cat','2015 mult', '2016 cat', '2016 mult'), pch = c(21, 22, 16, 15))
 
 
 # bird food orders
