c524cb963b60d05af29f0b3dab7795af318a5e33
tmckinno <tmckinno@dhcp-v880-2-04332.1Xwireless.unc.edu>
2017-01-24 12:37:24 -0500

    fix rankings plot

--- northern_comparisons.R
+++ northern_comparisons.R
@@ -12,7 +12,6 @@ dat = read.csv('HubbardBrookData/Copy of leps.csv', header=T, stringsAsFactors =
 singer = read.csv("SingerData/SingerData.csv", stringsAsFactors = F)
 
 #source data
-setwd("~/Desktop/caterpillars-count-analysis")
 source("tree_species_model.R")
 
 #improve format/put tree species names into common names
@@ -24,7 +23,7 @@ dat$tree.name = ifelse(dat$tree.spec == 1, "American beech",
                 ifelse(dat$tree.spec == 3, "Striped maple",
                 ifelse(dat$tree.spec == 4, "Viburnum", NA))))
 singer$hostplantspecies = gsub("Hammamelis virginiana", "Hamamelis virginiana", singer$hostplantspecies) #incorrect spelling
-plant_codes1 = rename(plant_codes, hostplantspecies = TreeSciName)
+plant_codes1 = dplyr::rename(plant_codes, hostplantspecies = TreeSciName)
 singer1 = singer %>% left_join(plant_codes1, by = "hostplantspecies") %>% select(-TreeCode, -Notes)
 
 #remove data from 1994 because it had far more caterpillars than any other year
@@ -35,7 +34,7 @@ dat1 = dplyr::filter(dat, year!="1994")
 sumbysp = dat1 %>% group_by(tree.name) %>% 
   dplyr::summarize(sum_count = sum(number.lep), biomass = sum(lepbio.mass.mg.)) 
 hubbard_ranks = sumbysp[order(sumbysp$sum_count, decreasing = T),]
-hubbard_ranks_sel = hubbard_ranks %>% rename(ComName = tree.name) %>% filter(ComName %in% c("Sugar maple", "American beech"))
+hubbard_ranks_sel = hubbard_ranks %>% dplyr::rename(ComName = tree.name) %>% filter(ComName %in% c("Sugar maple", "American beech"))
 hubbard_ranks_sel = cbind(hubbard_ranks = rownames(hubbard_ranks_sel), hubbard_ranks_sel)
 
 #subset dataset to only include 96/97 data to see if there is effect on beech and sugar maple amounts
@@ -48,7 +47,7 @@ surveysct = data.frame(table(surveys)) %>% filter(Freq>0)
 
 #Find patterns in Singer Data 
 #include only trees without a treatment
-singer_means = singer1 %>% filter(treatment == "unbagged") %>% group_by(ComName) %>% summarize(mean_cat_dens = mean(generalist.density.m2)) 
+singer_means = singer1 %>% filter(treatment == "unbagged") %>% group_by(ComName) %>% dplyr::summarize(mean_cat_dens = mean(generalist.density.m2)) 
 singer_ranks = singer_means[order(singer_means$mean_cat_dens, decreasing = T),]
 singer_ranks = cbind(singer_ranks = rownames(singer_ranks), singer_ranks)
 singer_ranks$ComName = gsub("Northern red oak", "Red oak", singer_ranks$ComName)
@@ -59,7 +58,7 @@ vis$cats_count = ifelse(vis$arthCode == "LEPL", vis$count, 0)
 
 #group by unique surveys and summarize by caterpillar density
 #vis_norm = filter(vis, cats_count <= 5) #use if you want to exclude big colonies
-south_means = vis %>% rename(ComName = clean_plantSp) %>% group_by(ComName) %>% dplyr::summarize(mean_cat_dens = mean(cats_count))
+south_means = vis %>% dplyr::rename(ComName = clean_plantSp) %>% group_by(ComName) %>% dplyr::summarize(mean_cat_dens = mean(cats_count))
 south_ranks = south_means[order(south_means$mean_cat_dens, decreasing = T),]
 south_relevant = filter(south_ranks, ComName %in% c("Red oak","Red maple", "Witch hazel", "Sweet birch", "American beech", "Sugar maple"))
 south_relevant = cbind(south_ranks = rownames(south_relevant), south_relevant)
@@ -68,8 +67,15 @@ south_relevant = cbind(south_ranks = rownames(south_relevant), south_relevant)
 # convert rownames (ranking numbers) to a column (y-axis of figure)
 # merge together the 3 data sets by tree species
 south_singer = left_join(south_relevant, singer_ranks, by = "ComName")
-all_singer = left_join(south_singer, hubbard_ranks_sel, by = "ComName") 
-plot(all_singer$ComName, all_singer$south_ranks) # 
+all_ranks = south_singer %>% left_join(hubbard_ranks_sel, by = "ComName") 
+#create columns that allow for numeric plotting
+all_ranks$singer_cat = 1
+all_ranks$south_cat = 2
+all_ranks$hubbard_cat = 3
+plot(all_ranks$south_cat, all_ranks$south_ranks, xlim = c(.2,3.6), ylim = c(0,6), type = "n", xlab = "Location", ylab = "Relative Rank of Caterpillar Abund.") # needs work
+  text(all_ranks$south_cat, all_ranks$south_ranks, labels = all_ranks$ComName, cex = .6)
+  text(all_ranks$singer_cat, as.numeric(all_ranks$singer_ranks)-3, labels = all_ranks$ComName, cex = .6)
+  text(all_ranks$hubbard_cat, all_ranks$hubbard_ranks, labels = all_ranks$ComName, cex = .6)
 
 
 
