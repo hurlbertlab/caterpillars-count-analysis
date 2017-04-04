890ae4c7d9c911fe0c513038294c284c4961a240
tmckinno <tmckinno@live.unc.edu>
2017-04-04 12:46:38 -0400

    minor changes

--- tree_species_model.R
+++ tree_species_model.R
@@ -147,7 +147,7 @@ names(leaf_tri1) = c("TreeCodes", "leaf_area_cm2")
 all_leaves = rbind(leaf_app_clean1, leaf_tri1)
   
 leaves_grouped =  group_by(all_leaves, TreeCodes) 
-leaves_sp = dplyr::summarize(leaves_grouped, (avg_leaf_area_cm2 = mean(leaf_area_cm2)))
+leaves_sp = dplyr::summarize(leaves_grouped, (mean(leaf_area_cm2)))
 
 #merge avg leaf area app/species with common names 
 leaves_sp1 = merge(leaves_sp, plant_codes, by.x = "TreeCodes", by.y = "TreeCode", all.x=T)
@@ -161,8 +161,8 @@ missingnames = c("ACPU", "ACRV", "ACSU","ARSP", "ASCA", "ASH", "CADRE","CAKYA", 
                  ,"COPL","ELUM","FLAV","GUAR","GUSP","HALA","ILAM","LIAM","LIST","MAER", "NYSA", 
                  "NYSS", "OYAR", "POPS", "PRUNIS", "PUMO", "QUAO","QUMI", "RASPBERRY", "RH MA", "RHAR",
                  "SAAB","SYOR","TICO","TORA")
-for (code in missingnames) {
-  print(sum(leaf_app$TreeSpecies == code))}
+#for (code in missingnames) {
+#  print(sum(leaf_app$TreeSpecies == code))}
 
 #merge avg leaf area with arth density data
 food_count_merged2 = merge(food_count_merged1, leaves_sp1, by.x = "plantSpecies", by.y="ComName", all.x = T)
@@ -270,81 +270,31 @@ barplot(plotting.log_tri_caterpillar$means, names.arg=plotting.log_tri_caterpill
          ylim = c(-.3,.4), cex.names=.65, cex.axis = .75, 
         col = c("darkblue", "darkblue", "blue", "deepskyblue3", "deepskyblue2", "deepskyblue1", "deepskyblue1", "deepskyblue1", "deepskyblue1", "aliceblue"))
 text(x=seq(from=.7, to= 11.5 ,by=1.2), y=.35, plotting.log_tri_caterpillar$M)
-#if then statement for the colors?
 
-#-----------------------------------------------------------------------test for how often you see an arthropod density at least this extreme-----------------------
-#create columns with Y/N data for certain arth density thresholds
-count_merged3 = count_merged2
-count_merged3$percent40 = ifelse(count_merged3$sum_count >= 40, "Yes", "No")
-count_merged3$percent10 = ifelse(count_merged3$sum_count >= 10, "Yes", "No")
-count_merged3$percent5 = ifelse(count_merged3$sum_count >= 5, "Yes", "No")
-count_merged3$percent1 = ifelse(count_merged3$sum_count >= 1, "Yes", "No")
+#max and min average normalized values for each tree species
+app_food_range= count_common_app_food %>%
+               group_by(plantSpecies) %>%
+               dplyr::summarize(mean = mean(count_norm)) %>%
+               filter(plantSpecies != "Carolina silverbell") %>%
+               dplyr::summarize(min = min(mean), max = max(mean))
 
-#get percentage of each tree species that meet the density criteria for each threshold 
-percent40 = count_merged3 %>% group_by(plantSpecies) %>% dplyr::summarise(perc40 = sum(percent40=="Yes")/length(percent40))
-percent40 = data.frame(percent40)
-percent40$not40 = 1-percent40$perc40
-
-percent10 = count_merged3 %>% group_by(plantSpecies) %>% dplyr::summarise(perc10=sum(percent10=="Yes")/length(percent10))
-percent10 = data.frame(percent10)
-percent10$not10 = 1-percent10$perc10
-
-percent5 = count_merged3 %>% group_by(plantSpecies) %>% dplyr::summarise(perc5 = sum(percent5=="Yes")/length(percent5))
-percent5 = data.frame(percent5)
-percent5$not5 = 1-percent5$perc5
-
-percent1 = count_merged3 %>% group_by(plantSpecies) %>% dplyr::summarise(perc1 = sum(percent1=="Yes")/length(percent1))
-percent1 = data.frame(percent1)
-percent1$not1 = 1-percent1$perc1
-
-#create df with all threshold percentages for all species
-percent40.10 = merge(percent40, percent10, by="plantSpecies")
-percent40.10.5 = merge(percent40.10, percent5, by = "plantSpecies")
-extremes_bysp = merge(percent40.10.5, percent1, by = "plantSpecies")
-
-#merge threshold percentages with avg leaf areas per species
-extremes_bysp1 = merge(extremes_bysp, leaves_sp1, by.x = "plantSpecies", by.y= "ComName") #decrease in trees here- why???? 29 out of 52 possible -> ones we don't have leaf areas for?
-extremes_24 = dplyr::filter(extremes_bysp1, plantSpecies %in% common_24$trees) #missing two of these-> why?
-
-#merge plant codes back in for plotting purposes
-extremes_241=merge(extremes_24, plant_codes, by.x = "plantSpecies", by.y = "ComName", all.x = TRUE)
-extremes_241 = dplyr::select(extremes_241, -TreeSciName, -Notes)
-
-#plot relationship between likelihood of seeing an extreme arth dens and leaf area 
-#(tree species above and below the curve tell us which species are good quality, whcih species are poorer quality)
-
-par(mfrow = c(2, 2), mar = c(5, 5, 1, 1))
-lm.40 =lm(extremes_241$perc40 ~ extremes_241$avg_leaf_area_cm2)
-p40=plot(extremes_241$avg_leaf_area_cm2, extremes_241$perc40, ylim=c(0,1), xlab = "Mean Leaf Area (cm2)", ylab = "Percent of Surveys >=40 Arth", cex.axis = .6)
-abline(lm.40)
-
-plot(extremes_241$avg_leaf_area_cm2, extremes_241$perc10, ylim=c(0,1), xlab = "Mean Leaf Area (cm2)", ylab = "Percent of Surveys >=10 Arth")
-lm.10 =lm(extremes_241$perc10 ~ extremes_241$avg_leaf_area_cm2)
-abline(lm.10)
-
-par(mfrow = c(1, 1), mar = c(5, 5, 1, 1))
-plot5=plot(extremes_241$avg_leaf_area_cm2, extremes_241$perc5, ylim=c(0,.6), xlab = "Mean Leaf Area (cm2)", ylab = "Proportion of Surveys >=5 Arth", col = "aquamarine", pch=16)
-lm.5 =lm(extremes_241$perc5 ~ extremes_241$avg_leaf_area_cm2)
-abline(lm.5)
-text(extremes_241$avg_leaf_area_cm2, extremes_241$perc5, labels = extremes_241$TreeCode, cex = .7)
+app_caterpillar_range = count_common_app_caterpillar %>%
+                       group_by(plantSpecies) %>%
+                       dplyr::summarize(mean = mean(count_norm)) %>%
+                       filter(plantSpecies != "Carolina silverbell") %>%
+                       dplyr::summarize(min = min(mean), max = max(mean))
+tri_food_range = count_common_tri_food %>%
+                group_by(plantSpecies) %>%
+                dplyr::summarize(mean = mean(count_norm)) %>%
+                filter(plantSpecies != "Pin oak") %>%
+                dplyr::summarize(min = min(mean), max = max(mean))
+tri_caterpillar_range = count_common_tri_caterpillar %>%
+                       group_by(plantSpecies) %>%
+                       dplyr::summarize(mean = mean(count_norm)) %>%
+                       filter(plantSpecies != "Pin oak") %>%
+                       dplyr::summarize(min = min(mean), max = max(mean))
 
 
-plot(extremes_241$avg_leaf_area_cm2, extremes_241$perc1, ylim=c(0,1), xlab = "Mean Leaf Area (cm2)", ylab = "Percent of Surveys >=1 Arth")
-lm.1 =lm(extremes_241$perc1 ~ extremes_241$avg_leaf_area_cm2)
-abline(lm.1)
-
-#chi squared data reshaping
-#table40 = table(count_merged3$percent40, count_merged3$plantSpecies)
-#table10 = table(count_merged3$percent10, count_merged3$plantSpecies)
-#table5 = table(count_merged3$percent5, count_merged3$plantSpecies)
-#table1 = table(count_merged3$percent1, count_merged3$plantSpecies)
-#chisq.test(table40)
-#chisq.test(table10)
-#chisq.test(table5)
-#chisq.test(table1)
-
-#so the above results tell me that the number of arthropods seen at each threshold is dependent on 
-#tree species (is this true for all tree species)?
 
 
 
